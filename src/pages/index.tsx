4a887a0fd7895f070d1b4f7777e46f5637d8acae
sh-okada <sh-okada@ymail.ne.jp>
2023-12-24 01:58:56 +0900

    リファクタリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,5 @@
-import { Grid } from "@mui/material";
-import { useEffect, useState } from "react";
-
-import SampleCard from "@/components/molecules/SampleCard";
+import SampleList from "@/components/organisms/SampleList";
 
 export default function Page() {
-  const [data, setData] = useState<any[]>();
-
-  useEffect(() => {
-    fetch("/api/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, []);
-
-  return (
-    <Grid container spacing={2}>
-      {data &&
-        data.map((v) => (
-          <Grid key={v.id} item xs={12} md={6}>
-            <SampleCard
-              firstName={v.firstName}
-              lastName={v.lastName}
-              department={v.department.name}
-            />
-          </Grid>
-        ))}
-    </Grid>
-  );
+  return <SampleList />;
 }
