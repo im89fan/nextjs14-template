aefac350022cc948e629497fabe78ce996b80950
sh-okada <sh-okada@ymail.ne.jp>
2023-12-24 01:35:42 +0900

    レイアウト修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,31 @@
-import { Typography } from "@mui/material";
+import { Grid } from "@mui/material";
+import { useEffect, useState } from "react";
+
+import SampleCard from "@/components/molecules/SampleCard";
 
 export default function Page() {
+  const [data, setData] = useState<any[]>();
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
   return (
-    <>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-      <Typography>コンテンツ</Typography>
-    </>
+    <Grid container spacing={2}>
+      {data &&
+        data.map((v) => (
+          <Grid key={v.id} item xs={12} md={6}>
+            <SampleCard
+              firstName={v.firstName}
+              lastName={v.lastName}
+              department={v.department.name}
+            />
+          </Grid>
+        ))}
+    </Grid>
   );
 }
