6c0bf2a0573960dbccacfe4748636167c73aa588
sh-okada <sh-okada@ymail.ne.jp>
2023-12-24 09:03:36 +0900

    データ修正

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -18,63 +18,15 @@ export default async function handler(
   res.status(200).json([
     {
       id: 1,
-      firstName: "R",
-      lastName: "指定",
+      firstName: "田中",
+      lastName: "太郎",
       department: { id: 1, name: "営業部" },
     },
     {
       id: 2,
-      firstName: "呂布",
-      lastName: "カルマ",
+      firstName: "山田",
+      lastName: "花子",
       department: { id: 1, name: "営業部" },
-    },
-    {
-      id: 3,
-      firstName: "DOTA",
-      lastName: "MA",
-      department: { id: 2, name: "人事部" },
-    },
-    {
-      id: 4,
-      firstName: "T",
-      lastName: "Pablow",
-      department: { id: 2, name: "人事部" },
-    },
-    {
-      id: 5,
-      firstName: "KT",
-      lastName: "ちゃん",
-      department: { id: 2, name: "人事部" },
-    },
-    {
-      id: 6,
-      firstName: "梵",
-      lastName: "頭",
-      department: { id: 2, name: "人事部" },
-    },
-    {
-      id: 7,
-      firstName: "SA",
-      lastName: "M",
-      department: { id: 2, name: "人事部" },
-    },
-    {
-      id: 8,
-      firstName: "MA",
-      lastName: "KA",
-      department: { id: 2, name: "人事部" },
-    },
-    {
-      id: 9,
-      firstName: "CHE",
-      lastName: "HON",
-      department: { id: 2, name: "人事部" },
-    },
-    {
-      id: 10,
-      firstName: "韻",
-      lastName: "マン",
-      department: { id: 2, name: "人事部" },
     },
   ]);
 }
